a59e709e0add899e7686a369e30027af25ee922c
Ranee <raneek0326@gmail.com>
2023-12-26 20:14:16 +0900

    전처리완료

--- raw_data/RawDataEdit.py
+++ raw_data/RawDataEdit.py
@@ -43,3 +43,24 @@ result.to_csv("평균연령.csv", encoding = 'ANSI')
 # 병합한 csv 파일 정렬 및 편집
 
 merged_csv = pd.read_csv('C:\\Users\\MSI\\Desktop\\depression-project\\Ranee\\raw_data\\연령,스트레스인지율병합.csv', encoding='ANSI')
+
+
+
+age = pd.read_csv(r'C:\Users\MSI\Desktop\depression-project\Ranee\0_Data\age&stress.csv', encoding='ANSI')
+population = pd.read_csv(r'C:\Users\MSI\Desktop\depression-project\Ranee\0_Data\population&employment.csv', encoding='ANSI')
+others = pd.read_csv(r'C:\Users\MSI\Desktop\depression-project\Ranee\0_Data\others.csv', encoding='ANSI')
+default_index = pd.read_csv(r'C:\Users\MSI\Desktop\depression-project\Ranee\raw_data\지자체구분.csv', encoding = 'ANSI')
+
+age.info
+age.isnull()
+population.info
+population = population.drop(['시도','시군구'], axis='columns')
+others.info
+others.isnull()
+others = others.drop(['시도','시군구'], axis='columns')
+default_index = default_index.drop(['Unnamed: 3','Unnamed: 4'],axis=1)
+a = pd.merge(default_index,age, on='행정번호')
+b = pd.merge(a,population, on=['행정번호','년도'])
+final_data = pd.merge(b,others, on=['행정번호','년도'])
+final_data.to_csv("final.csv", encoding = 'ANSI')
+
